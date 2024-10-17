b5b462d3a89c94c66b0245b2831912c546d79f1f
Xyndra <71836523+Xyndra@users.noreply.github.com>
2024-10-17 16:33:34 +0200

    Works

--- src/main/java/net/hollowed/wikwriv/mixin/MinecraftServerMixin.java
+++ src/main/java/net/hollowed/wikwriv/mixin/MinecraftServerMixin.java
@@ -1,6 +1,7 @@
 package net.hollowed.wikwriv.mixin;
 
 import de.xyndra.wikwriv.ResourceUtil;
+import net.hollowed.wikwriv.WicksWrivets;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Final;
@@ -10,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -23,13 +25,16 @@ public class MinecraftServerMixin {
         Path worldPath = Path.of("").resolve("saves").resolve(session.getDirectory().path());
         // Copy dimension data from resources to world folder
         Path dimensionDataPath = worldPath.resolve("dimensions").resolve("wikwriv").resolve("wikwriv");
+        if (worldPath.toString().toLowerCase().contains("dev") && Files.exists(dimensionDataPath)) {
+            WicksWrivets.LOGGER.info("Skipping dimension data copy because this is a dev world");
+            return;
+        }
         if (!dimensionDataPath.toFile().exists()) {
             dimensionDataPath.toFile().mkdirs();
         }
         String resourcePath = "/dimension_data/wikwriv";
         List<String> resources = ResourceUtil.INSTANCE.getFilesInResourceDir(resourcePath);
         for (String resource : resources) {
-            System.out.println("Copying " + resource + " to " + dimensionDataPath);
             ResourceUtil.INSTANCE.copyResourceToDir(resource, resourcePath, dimensionDataPath);
         }
     }
